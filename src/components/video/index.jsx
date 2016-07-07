d222973922099b3502cef3b4687132bcc951f8a3
Shiquan Sun <me@imsun.net>
2016-07-07 15:26:31 +0800

    support volume control

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -9,11 +9,14 @@ import classNames from 'classnames'
 
 import Paper from 'material-ui/Paper'
 import Slider from 'material-ui/Slider'
+import Popover from 'material-ui/Popover'
 import IconButton from 'material-ui/IconButton'
 import AvPlayIcon from 'material-ui/svg-icons/av/play-arrow'
 import AvPauseIcon from 'material-ui/svg-icons/av/pause'
 import FullscreenIcon from 'material-ui/svg-icons/navigation/fullscreen'
 import ExitFullscreenIcon from 'material-ui/svg-icons/navigation/fullscreen-exit'
+import VolumeUpIcon from 'material-ui/svg-icons/av/volume-up'
+import VolumeOffIcon from 'material-ui/svg-icons/av/volume-off'
 import TextField from 'material-ui/TextField'
 
 function toHHMMSS(secNum, spliter = ':') {
@@ -39,7 +42,8 @@ const initState = {
 	videoReady: false,
 	textToBeSent: '',
 	timer: null,
-	hideVideoControl: false
+	hideVideoControl: false,
+	volumeBarOpen: false
 }
 
 class Video extends React.Component {
@@ -54,6 +58,8 @@ class Video extends React.Component {
 			, 'toggleFullScreen', 'fullScreenChangeListener'
 			, 'showVideoControl', 'flashVideoControl'
 			, 'setHideVideoControlTimer', 'clearHideVideoControlTimer'
+			, 'toggleMuted', 'openVolumeBar', 'closeVolumeBar'
+			, 'handleVolumeChange'
 		]
 			.forEach(method => this[method] = this[method].bind(this))
 		if (fullScreenEnabled) {
@@ -76,6 +82,12 @@ class Video extends React.Component {
 		}
 		if (this.props.fullScreen !== prevProps.fullScreen) {
 			this.setHideVideoControlTimer()
+		}
+		if (this.props.muted !== prevProps.muted) {
+			this.target.muted = this.props.muted
+		}
+		if (this.props.volume !== prevProps.volume) {
+			this.target.volume = this.props.volume
 		}
 	}
 	componentDidMount() {
@@ -241,6 +253,28 @@ class Video extends React.Component {
 			clearTimeout(this.__hideVideoControlTimer)
 		}
 	}
+	toggleMuted() {
+		this.props.set({
+			muted: !this.props.muted
+		})
+	}
+	openVolumeBar() {
+		if (!this.state.volumeBarOpen) {
+			this.setState({
+				volumeBarOpen: true
+			})
+		}
+	}
+	closeVolumeBar() {
+		this.setState({
+			volumeBarOpen: false
+		})
+	}
+	handleVolumeChange(e) {
+		this.props.set({
+			volume: e.target.value
+		})
+	}
 	render() {
 		const smallStyle = {
 			width: 30,
@@ -250,6 +284,10 @@ class Video extends React.Component {
 		const iconLarge = {
 			width: 30,
 			height: 30
+		}
+		const iconMedium = {
+			width: 24,
+			height: 24
 		}
 		return (
 			<Paper className={classNames('video', {
@@ -261,6 +299,21 @@ class Video extends React.Component {
 					onTouchTap={this.flashVideoControl}
 					onMouseMove={this.flashVideoControl}
 				/>
+				<div
+					className="text-sender"
+					onmouseover={this.showVideoControl}
+				>
+					<TextField
+						id="inputField"
+						className="input-field"
+						underlineShow={false}
+						hintText="You can text here"
+						onChange={this.onTextChange}
+						value={this.state.textToBeSent}
+						onKeyDown={e => { if (e.keyCode === 13) this.send() }}
+					/>
+					<a className="submit-button" onTouchTap={this.send}>SEND</a>
+				</div>
 				<div
 					className="control-bar"
 					onMouseOver={this.showVideoControl}
@@ -288,11 +341,43 @@ class Video extends React.Component {
 					<span className="video-time">
 						{toHHMMSS(this.state.currentTime)}/{toHHMMSS(this.state.duration)}
 					</span>
+					<div>
+						<IconButton
+							style={smallStyle}
+							iconStyle={iconMedium}
+							onTouchTap={this.toggleMuted}
+							onMouseOver={this.openVolumeBar}
+							onMouseLeave={this.closeVolumeBar}
+						>
+							{
+								this.props.muted
+									? <VolumeOffIcon />
+									: <VolumeUpIcon />
+							}
+						</IconButton>
+						<Paper
+							className={classNames('volume-bar', {
+								hidden: !this.state.volumeBarOpen
+							})}
+							onMouseOver={this.openVolumeBar}
+							onMouseLeave={this.closeVolumeBar}
+						>
+							<input
+								className="volume-slider"
+								type="range"
+								min="0"
+								max="1"
+								step="0.1"
+								value={this.props.volume}
+								onChange={this.handleVolumeChange}
+							/>
+						</Paper>
+					</div>
 					{
 						fullScreenEnabled && (
 							<IconButton
 								style={smallStyle}
-								iconStyle={iconLarge}
+								iconStyle={iconMedium}
 								onTouchTap={this.toggleFullScreen}
 							>
 								{
@@ -303,21 +388,6 @@ class Video extends React.Component {
 							</IconButton>
 						)
 					}
-				</div>
-				<div
-					className="text-sender"
-					onmouseover={this.showVideoControl}
-				>
-					<TextField
-						id="inputField"
-						className="input-field"
-						underlineShow={false}
-						hintText="You can text here"
-						onChange={this.onTextChange}
-						value={this.state.textToBeSent}
-						onKeyDown={e => { if (e.keyCode === 13) this.send() }}
-					/>
-					<a className="submit-button" onTouchTap={this.send}>SEND</a>
 				</div>
 			</Paper>
 		)
--- src/reducers/video.js
+++ src/reducers/video.js
@@ -1,16 +1,27 @@
 import { videoActions } from '../actions'
 
+if (isNaN(parseFloat(localStorage.getItem('videoVolume')))) {
+	localStorage.setItem('videoVolume', 0.5)
+}
+
 const initState = {
 	paused: true,
 	currentTime: 0,
-	fullScreen: false
+	fullScreen: false,
+	mute: false,
+	volume: parseFloat(localStorage.getItem('videoVolume')),
+	muted: false
 }
 
 export default function(state = initState, action) {
 	switch (action.type) {
 		case videoActions.SET:
+			if (!isNaN(parseFloat(action.data.volume))) {
+				localStorage.setItem('videoVolume', action.data.volume)
+			}
 			return Object.assign({}, state, action.data)
 		case videoActions.SEEK:
+			console.log('seektime ........', action.currentTime)
 			return Object.assign({}, state, {
 				currentTime: action.currentTime
 			})
