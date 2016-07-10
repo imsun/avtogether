8c90d612646a293062c1dea20a28d37ccf99804c
Shiquan Sun <me@imsun.net>
2016-07-10 14:17:27 +0800

    add `setWithoutBroadcast`

--- src/reducers/video.js
+++ src/reducers/video.js
@@ -15,20 +15,31 @@ const initState = {
 	isLoading: false,
 	duration: 0,
 	videoReady: false,
-	realTime: 0
+	realTime: 0,
+	disableBroadcast: false
+}
+
+function onSet(state, action, extraData = {}) {
+	const data = Object.assign({}, action.data, extraData)
+	if (!isNaN(parseFloat(data.volume))) {
+		localStorage.setItem('videoVolume', data.volume)
+	}
+	if (data.currentTime !== undefined) {
+		data.realTime = data.currentTime
+	}
+	return Object.assign({}, state, data)
 }
 
 export default function(state = initState, action) {
 	switch (action.type) {
 		case videoActions.SET:
-			const data = Object.assign({}, action.data)
-			if (!isNaN(parseFloat(data.volume))) {
-				localStorage.setItem('videoVolume', data.volume)
-			}
-			if (data.currentTime !== undefined) {
-				data.realTime = data.currentTime
-			}
-			return Object.assign({}, state, data)
+			return onSet(state, action, {
+				disableBroadcast: false
+			})
+		case videoActions.SET_WITHOUT_BROADCAST:
+			return onSet(state, action, {
+				disableBroadcast: true
+			})
 		case videoActions.PUSH_STATUS:
 			return Object.assign({}, state, {
 				statusStack: state.statusStack.concat([action.status])
